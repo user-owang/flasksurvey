fac7495317db1211ef071898271eebf06a9be433
unknown <hilow0090@gmail.com>
2023-11-17 22:49:27 -0800

    finished survey

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
-from flask import flask, request, render_template
+from flask import *
 
 from surveys import *
 
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
+app.debug = True
 
 responses = []
 
-app.route('/')
+@app.route('/')
 def showHome():
-  
+  return render_template('home.html', surveys=surveys)
+
+@app.route('/survey/<survey_id>')
+def showSurvey(survey_id):
+  return render_template('survey.html', survey = surveys.get(survey_id), survey_id = survey_id)

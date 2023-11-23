a22a95ded00b6244a70e7c8af8b07997831a2e18
unknown <hilow0090@gmail.com>
2023-11-22 17:24:26 -0800

    added session storage

--- app.py
+++ app.py
@@ -6,40 +6,45 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 app.debug = True
+app.config['SECRET_KEY'] = 'abcdefghijklmnopqrstuvwxyz'
 
-responses = dict()
+
 
 @app.route('/')
 def showHome():
+  for test in surveys:
+    if not session.get(test):
+      session[test] = list()
   return render_template('home.html', surveys=surveys)
 
 @app.route('/survey/<survey_id>')
 def showSurvey(survey_id):
+  if len(session.get(survey_id)) == len(surveys.get(survey_id).questions):
+    return redirect(f'/survey/{survey_id}/thanks')
+  else:
+    return redirect(f'/survey/{survey_id}/question/{len(session.get(survey_id))}')
   return render_template('survey.html', survey = surveys.get(survey_id), survey_id = survey_id)
 
 @app.route('/survey/<survey_id>/question/<int:index>')
 def showQuestion(survey_id, index):
-  if responses.get(survey_id) and index > len(responses.get(survey_id)):
-    redirect(f'/survey/{survey_id}/question/{len(responses.get(survey_id))}')
-    return flash('You have tried to access questions out of order. Please continue where you left off.')  
-  if not responses.get(survey_id) and index>0:
-    redirect(f'/survey/{survey_id}/question/0')
+  if index < len(session.get(survey_id)):
+    redirect(f'/survey/{survey_id}/question/{len(session.get(survey_id))}')
     return flash('You have tried to access questions out of order. Please continue where you left off.')
-  if len(surveys.get(survey_id)['questions']) == index and responses.get(survey_id) and len(responses.get(survey_id)) == index:
-    return redirect('/thanks')
+  if len(surveys[survey_id].questions) == index and len(session.get(survey_id)) == index:
+    return redirect(f'/survey/{survey_id}/thanks')
   return render_template('question.html', survey = surveys.get(survey_id), survey_id = survey_id, i = index)
 
 @app.route('/survey/<survey_id>/responses', methods=['POST'])
 def addResponse(survey_id):
   answer = request.form['question']
   text = request.form.get('text')
-  if not responses.get(survey_id):
-    responses[survey_id] = []
   if text:
     answer = {'answer': request.form['question'], 'text': text}
-  responses[survey_id].append(answer)
-  return redirect(f'question/{len(responses[survey_id])}')
+  temp = session[survey_id]
+  temp.append(answer)
+  session[survey_id] = temp
+  return redirect(f'question/{len(session[survey_id])}')
 
-@app.route('/thanks')
-def showThanks():
-  return render_template('thanks.html')
+@app.route('/survey/<survey_id>/thanks')
+def showThanks(survey_id):
+  return render_template('thanks.html', survey = surveys.get(survey_id), test = survey_id, indexes = list(range(0,len(surveys.get(survey_id).questions))))
